f6a99e81f5dc80dd5781cb31012ec4a3e41d86f3
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2020-04-06 10:57:20 +0200

    Update test-periods.R

--- testthat/test-periods.R
+++ testthat/test-periods.R
@@ -2,7 +2,7 @@ library(testthat)
 library(dplyr)
 context("checks that period values are valid")
 
-base_data <- read.csv('../data/rodent_abundance.csv', 
+base_data <- read.csv('../data/data.csv', 
                       stringsAsFactors = F)
 
 test_that("period numbers are valid", {
